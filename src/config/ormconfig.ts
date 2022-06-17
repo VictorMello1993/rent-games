925e7e04e0390b13764cfbaecbc64c5272464271
Victor Mello <victor.mello@al.infnet.edu.br>
2022-06-16 23:34:10 -0300

    Tentando corrigir o erro na comunicação do container com TypeOrm

---
+++ src/config/ormconfig.ts
@@ -0,0 +1,21 @@
+import { DataSource } from 'typeorm';
+
+export const AppDataSource = new DataSource({
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'victor',
+  password: 'nest123',
+  database: 'dev',
+  migrations: [__dirname + '..' + 'infra/migrations/*{.ts,.js}'],
+  entities: [__dirname + '..' + 'core/entities/*{.ts,.js}'],
+  synchronize: true,
+});
+
+AppDataSource.initialize()
+  .then(() => {
+    console.log('Data Source has been initialized!');
+  })
+  .catch((err) => {
+    console.error('Error during Data Source initialization', err);
+  });
---
+++ src/infra/migrations/1655423275965-CreateUser.ts
@@ -0,0 +1,48 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateUser1655420942180 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'users',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+          },
+          {
+            name: 'password',
+            type: 'varchar',
+          },
+          {
+            name: 'email',
+            type: 'varchar',
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+          },
+          {
+            name: 'admin',
+            type: 'boolean',
+            default: false,
+          },
+          {
+            name: 'createdAt',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'telephone',
+            type: 'varchar',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('users');
+  }
+}
