410b4e7080d3e2e6ea35cca7828fb4974d5b95f9
Victor Mello <victor.mello@al.infnet.edu.br>
2022-08-25 22:02:11 -0300

    Protegendo a conexão com banco de dados com variáveis de ambiente

--- src/config/ormconfig.ts
+++ src/config/ormconfig.ts
@@ -10,9 +10,9 @@ import { Rental } from '../core/entities/Rental';
 import { AdminSeeder1656265574838 } from '../infra/migrations/1656265574838-  AdminSeeder';
 
 export const AppDataSource = new DataSource({
-  type: 'postgres',
-  host: 'localhost',
-  port: 5432,
+  type: 'mysql',
+  host: process.env.HOST,
+  port: Number(process.env.APP_PORT),
   username: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASSWORD,
   database: process.env.POSTGRES_DB,
